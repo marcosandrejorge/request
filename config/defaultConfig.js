6dfd534cbd7f0474d1454ed2947ffcca195bc55c
Marcos André Jorge <marcosandre.bnu@gmail.com>
2021-03-19 14:07:51 -0300

    03

---
+++ config/defaultConfig.js
@@ -0,0 +1,9 @@
+export default {
+    baseUrl: "",
+    headers: { "Content-Type": "application/json" },
+    timeout: 8000,
+    method: "",
+    data: {},
+    auth: {},
+    urlPath: ""
+}
--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-function requestService(){
-    return "Teste";
+import axios from "axios/dist/axios";
+
+const objConfig = require('./config/defaultConfig');
+
+const getConfig = (dataSource = null) => {
+    let baseUrl = dataSource.baseUrl;
+    let urlPath = dataSource.urlPath;
+    let data = dataSource.data;
+    let url = baseUrl + urlPath;
+    url = dataSource.url;
+
+    let config = {
+        url: url,
+        headers: dataSource.headers,
+        timeout: dataSource.timeout,
+        method: dataSource.method,
+        auth: dataSource.auth
+    }
+
+    if (config.method.toUpperCase() === 'GET') {
+        config.params = data;
+    } else {
+        config.data = data;
+    }
+
+    console.log(objConfig);
+
+    return config;
+}
+
+async function requestService(dataSource = null){
+    let config = getConfig(dataSource)
+    return new Promise((resolve, reject) => {
+        axios(config).then((response) => {
+            resolve(response)
+        })
+            .catch((error) => {
+                reject(error)
+            })
+    });
 }
 
 module.exports = requestService
