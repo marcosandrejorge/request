0b755f1ff9ca7bf6a278b7c7a089d6325b74d820
Marcos André Jorge <marcosandre.bnu@gmail.com>
2021-03-19 14:02:52 -0300

    02

--- index.js
+++ index.js
@@ -1,43 +1,5 @@
-import axios from "axios/dist/axios";
-
-const objConfig = {
-    baseUrl: "",
-    headers: { "Content-Type": "application/json" },
-    timeout: 8000,
-    method: "",
-    data: {},
-    auth: {},
-    urlPath: ""
-};
-
-const getConfig = (dataSource = null) => {
-    let baseUrl = dataSource.baseUrl;
-    let urlPath = dataSource.urlPath;
-    let data = dataSource.data;
-    let url = baseUrl + urlPath;
-    url = dataSource.url;
-
-    let config = {
-        url: url,
-        headers: dataSource.headers,
-        timeout: dataSource.timeout,
-        method: dataSource.method,
-        auth: dataSource.auth
-    }
-
-    if (config.method.toUpperCase() === 'GET') {
-        config.params = data;
-    } else {
-        config.data = data;
-    }
-
-    return config;
+function requestService(){
+    return "Teste";
 }
 
-function requestService(dataSource = null){
-    return dataSource;
-}
-
-module.exports = {
-    requestService
-}
+module.exports = requestService
